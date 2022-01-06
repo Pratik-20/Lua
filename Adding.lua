735e09fefaaa983be19d26fc120f8c33d8dfcbd6
Pratik Thorawade <86850437+Pratik-20@users.noreply.github.com>
2022-01-06 22:20:31 +0530

    Add files via upload

---
+++ Adding.lua
@@ -0,0 +1,16 @@
+print("Enter A = ")
+A = io.read("*n") 
+
+print("Enter B = ")
+B = io.read("*n") 
+
+C = A+B
+D = A-B
+E = A*B
+F = A/B
+
+print("Addition is = ",C)
+print("Substraction is = ",D)
+print("Multiplication is = ",E)
+print("Division is = ",F)
+print(" ")
